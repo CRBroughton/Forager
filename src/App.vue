37e8721628e1769c047a65e57e1e4c1791e84ea4
CRBroughton <crbroughton@posteo.uk>
2021-05-19 16:38:04 +0100

    added basic test map

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <l-map
+    v-model="zoom"
+    v-model:zoom="zoom"
+    :center="center"
+    :options="{ zoomSnap: 0.1, zoomDelta: 0.1 }"
+    @click="addMarker"
+  >
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    ></l-tile-layer>
+    <l-control-layers />
+
+    <l-marker
+      v-for="marker in markers"
+      :key="marker"
+      :lat-lng="marker"
+    ></l-marker>
+  </l-map>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LControlLayers,
+} from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
 
-export default defineComponent({
-  name: "App",
+export default {
   components: {
-    HelloWorld,
+    LMap,
+    LTileLayer,
+    LMarker,
+    LControlLayers,
   },
-});
+  data() {
+    return {
+      zoom: 16,
+      // iconWidth: 25,
+      // iconHeight: 40,
+      center: [50.8311091801179, -0.13301259444431282],
+      currentMarker: [],
+      markers: [[50.8311091801179, -0.13301259444431282]],
+    };
+  },
+  methods: {
+    addMarker(e) {
+      this.markers.push([e.latlng.lat, e.latlng.lng]);
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+}
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100vw;
 }
 </style>
