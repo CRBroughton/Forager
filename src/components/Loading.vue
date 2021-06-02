54fc13183caed381b619855d3997501260e778ef
CRBroughton <crbroughton@posteo.uk>
2021-06-02 16:08:21 +0100

    added Loading.vue component

---
+++ src/components/Loading.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-screen h-screen">
+    <div class="h-1/2 flex bg-red-100">
+      <img
+        class="m-auto"
+        src="https://via.placeholder.com/200"
+        alt=""
+        srcset=""
+      />
+    </div>
+    <div class="h-1/2 bg-red-300">
+      <div class="h-1/2 flex">
+        <h1 class="m-auto text-3xl">Forager</h1>
+      </div>
+      <div class="h-1/2 flex bg-yellow-400">
+        <h3 class="m-auto text-1xl">Loading...</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
