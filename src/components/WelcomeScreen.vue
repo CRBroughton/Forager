48cba0603cdaa752073df529b3ea529064b1fcc6
CRBroughton <crbroughton@posteo.uk>
2021-06-03 13:15:41 +0100

    improved home location popup

--- src/components/WelcomeScreen.vue
+++ src/components/WelcomeScreen.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>Please press your location</h1>
+  <div class="w-screen bottom-0 p-10">
+    <div class="bg-gray-100 rounded-full flex py-5 shadow-xl">
+      <h1 class="m-auto">Please click your location</h1>
+    </div>
   </div>
 </template>
 
