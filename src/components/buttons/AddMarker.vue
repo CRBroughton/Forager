b9b6394f991aad373db58fe7646ecb61d3ccd630
CRBroughton <crbroughton@posteo.uk>
2021-06-04 15:30:15 +0100

    added AddMarker.vue

--- src/components/buttons/AddMarker.vue
+++ src/components/buttons/AddMarker.vue
@@ -1,7 +1,25 @@
 <template>
-  <div></div>
+  <div v-if="!isHidden" class="bottom-2 right-2 w-screen flex flex-row-reverse">
+    <div class="flex w-60">
+      <MyButton title="Create"> Create </MyButton>
+      <MyButton title="Cancel" @click="isHidden = true"></MyButton>
+    </div>
+  </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import MyButton from "./BaseButton.vue";
+
+export default {
+  components: {
+    MyButton,
+  },
+  data() {
+    return {
+      isHidden: false,
+    };
+  },
+};
+</script>
 
 <style scoped></style>
