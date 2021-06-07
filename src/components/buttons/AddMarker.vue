140d7f04271506e9240a15b5a238dd6713e289c7
CRBroughton <crbroughton@posteo.uk>
2021-06-07 16:04:23 +0100

    added input field for marker

--- src/components/buttons/AddMarker.vue
+++ src/components/buttons/AddMarker.vue
@@ -1,8 +1,23 @@
 <template>
-  <div v-if="!isHidden" class="bottom-2 right-2 w-screen flex flex-row-reverse">
-    <div class="flex w-60">
-      <MyButton title="Create" class="mb-1 mr-1" @click="createMarker"> Create </MyButton>
-      <MyButton title="Cancel" class="mb-1 mr-1" @click="hidePopup"></MyButton>
+  <div v-if="!isHidden" class="bottom-2 right-2 w-auto">
+    <div class="flex m-auto flex-col items-center">
+      <input
+        type="text"
+        class="w-full flex rounded my-5 py-3 shadow-xl border-gray-200 border cursor-pointer outline-none focus:outline-none"
+        placeholder="Object Name"
+      />
+    </div>
+    <div class="flex flex-row-reverse0">
+      <div class="flex w-60">
+        <MyButton title="Create" class="mb-1 mr-1" @click="createMarker">
+          Create
+        </MyButton>
+        <MyButton
+          title="Cancel"
+          class="mb-1 mr-1"
+          @click="hidePopup"
+        ></MyButton>
+      </div>
     </div>
   </div>
 </template>
@@ -27,7 +42,7 @@ export default {
     },
     createMarker() {
       this.$emit("createMarker");
-    }
+    },
   },
 };
 </script>
