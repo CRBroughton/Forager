49a5e9aab1ea079afff6c4b3c3cda3c395a0877e
CRBroughton <crbroughton@posteo.uk>
2021-06-10 16:09:01 +0100

    improved options menu added icon to menu

--- src/components/OptionsMenu.vue
+++ src/components/OptionsMenu.vue
@@ -1,17 +1,62 @@
 <template>
-  <div class="w-full bottom-0 py-2 px-1 flex m-auto">
-    <div
-      class="w-full flex justify-center m-auto py-2 bg-gray-100 rounded-xl shadow-2xl"
-    >
-      <MyButton title="Delete Home" class="mb-1 mr-1 ml-1" @click="deleteHome">
-        Delete Home
-      </MyButton>
-      <MyButton title="Delete Markers" class="mb-1 mr-1" @click="deleteMarkers">
-        Delete Markers
-      </MyButton>
-      <MyButton title="Clear Cache" class="mb-1 mr-1" @click="clearCache">
-        Clear Cache
-      </MyButton>
+  <div class="w-full bottom-0 flex flex-wrap content-end sm:p-2 p-2">
+    <div class="w-full flex flex-col bg-gray-100 rounded-2xl shadow-2xl">
+      <div class="w-full pt-2 flex justify-center">
+        <div
+          @click="showOptions"
+          class="h-14 w-14 bg-white flex justify-center items-center rounded-full cursor-pointer"
+        >
+          <img src="" alt="" srcset="../assets/logo.png" class="w-10 h-10" />
+        </div>
+      </div>
+      <div
+        class="w-full p-2 flex justify-center items-center text-gray-500 font-semibold"
+      >
+        <p>Forager</p>
+      </div>
+      <div
+        class="w-full pb-2 flex justify-center items-center text-gray-500 text-xs"
+      >
+        <p>By Craig Broughton</p>
+      </div>
+      <div class="w-full h-full flex flex-wrap content-end">
+        <div class="w-full bottom-0 py-1 px-1">
+          <div
+            class="w-full flex justify-center m-auto py-2 bg-gray-200 rounded-xl"
+          >
+            <MyButton
+              title="Delete Home"
+              class="mb-1 mr-1 ml-1"
+              @click="deleteHome"
+            >
+              Delete Home
+            </MyButton>
+            <MyButton
+              title="Delete Markers"
+              class="mb-1 mr-1"
+              @click="deleteMarkers"
+            >
+              Delete Markers
+            </MyButton>
+            <MyButton title="Clear Cache" class="mb-1 mr-1" @click="clearCache">
+              Clear Cache
+            </MyButton>
+          </div>
+        </div>
+        <div class="w-full bottom-0 py-1 px-1">
+          <div
+            class="w-full flex justify-center m-auto py-2 bg-gray-200 rounded-xl"
+          >
+            <MyButton
+              title="Back"
+              class="mb-1 mr-1 ml-1 w-full"
+              @click="goBack"
+            >
+              Back
+            </MyButton>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -45,6 +90,10 @@ export default {
     },
     clearCache() {
       caches.delete("forager-cache");
+    },
+    goBack() {
+      this.isHidden = true;
+      this.$emit("hideOptions");
     },
   },
 };
