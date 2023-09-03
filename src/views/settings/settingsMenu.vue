aec009b190583c0c98b843df3fdd8326f1a51e60
CRBroughton <crbroughton@posteo.uk>
2023-09-03 13:18:19 +0100

    Revert "fix: :bug: increase blur amount for images menu"
    This reverts commit 604f94ae8d01d4d562ff5400043fdeb32336dab0.

--- src/views/settings/settingsMenu.vue
+++ src/views/settings/settingsMenu.vue
@@ -56,7 +56,7 @@ h1 {
     width: 100%;
     height: 100%;
     background: rgba(255,255,255,0.5);
-    backdrop-filter: blur(40px);
+    backdrop-filter: blur(20px);
     position: absolute;
     top: 0;
     left: 0;
