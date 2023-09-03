3d05b4887e37ec328de6a667f230868ff356d017
CRBroughton <crbroughton@posteo.uk>
2023-09-03 13:14:37 +0100

    fix: :bug: set bg to white to stop see through

--- src/views/settings/settingsMenu.vue
+++ src/views/settings/settingsMenu.vue
@@ -55,8 +55,7 @@ h1 {
     z-index: 1000;
     width: 100%;
     height: 100%;
-    background: rgba(255,255,255,0.5);
-    backdrop-filter: blur(40px);
+    background: white;
     position: absolute;
     top: 0;
     left: 0;
