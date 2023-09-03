b875fdeb156d9976af0fff7f5ee489bfc7e09848
CRBroughton <crbroughton@posteo.uk>
2023-09-03 13:18:05 +0100

    Revert "fix: :bug: set bg to white to stop see through"
    This reverts commit 3d05b4887e37ec328de6a667f230868ff356d017.

--- src/views/settings/settingsMenu.vue
+++ src/views/settings/settingsMenu.vue
@@ -55,7 +55,8 @@ h1 {
     z-index: 1000;
     width: 100%;
     height: 100%;
-    background: white;
+    background: rgba(255,255,255,0.5);
+    backdrop-filter: blur(40px);
     position: absolute;
     top: 0;
     left: 0;
