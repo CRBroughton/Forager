896008c2e3f2216464eb646921ce338d59592aab
CRBroughton <crbroughton@posteo.uk>
2023-09-09 20:13:53 +0100

    feat: :construction: add sliced view for adding new items, add expanded view

--- src/components/AddMarker.vue
+++ src/components/AddMarker.vue
@@ -59,11 +59,11 @@ function setSelectedItem(event: UserImage) {
       </div>
       <div class="m-auto items-center w-full">
         <ReferenceImages v-if="!creatingNewItem" :new="creatingNewItem" :images="user?.images" @change="setSelectedItem">
-          <div class="h-16 w-16 mb-1 bg-white active:bg-gray-100 flex justify-center items-center rounded-full shadow-xl border-gray-200 border cursor-pointer text-gray-500" @click="creatingNewItem = !creatingNewItem">
+          <div class="h-16 w-16 min-w-[4rem] min-h-[4rem] bg-white active:bg-gray-100 flex justify-center items-center rounded-full shadow-xl border-gray-200 border cursor-pointer text-gray-500" @click="creatingNewItem = !creatingNewItem">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2Z" /></svg>
           </div>
         </ReferenceImages>
-        <div v-else>
+        <div v-if="creatingNewItem">
           <div class="my-2">
             Selected Colour: {{
               selectedColour.length > 0 ? selectedColour : 'None selected'
--- src/components/ReferenceImages.vue
+++ src/components/ReferenceImages.vue
@@ -18,15 +18,37 @@ function selectImage(image: UserImage) {
   selectedImage.value = image
   emit('change', image)
 }
+
+const isSliced = ref(true)
 </script>
 
 <template>
-  <div class="images-container">
-    <slot />
-    <div v-for="image in props.images" :key="image.url" class="image-container">
+  <div v-if="isSliced" class="grid gap-4 grid-cols-3 my-4">
+    <div class="flex flex-col justify-center items-center">
+      <slot />
+      <p>Create</p>
+    </div>
+    <div v-for="image in props.images.slice(0, 5)" :key="image.url" class="flex flex-col justify-center items-center">
       <img class="image-icon" :class="{ selected: selectedImage?.url === image.url }" :src="image.url" @click="selectImage(image)">
       {{ image.name }}
     </div>
+  </div>
+  <div v-if="isSliced" class="w-full bg-white flex my-4 justify-center items-center rounded-xl" @click="isSliced = !isSliced">
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42Z" /></svg>
+  </div>
+
+  <div v-if="!isSliced" class="grid gap-4 grid-cols-3 my-4">
+    <div class="flex flex-col justify-center items-center">
+      <slot />
+      <p>Create</p>
+    </div>
+    <div v-for="image in props.images" :key="image.url" class="flex flex-col justify-center items-center">
+      <img class="image-icon" :class="{ selected: selectedImage?.url === image.url }" :src="image.url" @click="selectImage(image)">
+      {{ image.name }}
+    </div>
+  </div>
+  <div v-if="!isSliced" class="w-full bg-white flex my-4 justify-center items-center rounded-xl" @click="isSliced = !isSliced">
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6l-6 6l1.41 1.41Z" /></svg>
   </div>
 </template>
 
