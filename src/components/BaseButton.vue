1022b44c69d905404469c94a0d12ee25dadbb379
CRBroughton <crbroughton@posteo.uk>
2023-09-09 15:07:29 +0100

    feat: :art: add active state to button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <button class="bg-white py-4 px-8 w-full rounded-xl">
+  <button class="bg-white py-4 px-8 w-full rounded-xl active:bg-slate-100">
     <slot />
   </button>
 </template>
