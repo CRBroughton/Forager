9788645c25eac28f9657708d38458731d1069633
CRBroughton <crbroughton@posteo.uk>
2023-09-10 16:52:56 +0100

    refactor: :recycle: refactor items table migration to Go

--- db/main.go
+++ db/main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"log"
+	"os"
+	"strings"
 
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+
+	_ "github.com/crbroughton/forager/migrations"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		// serves static files from the /market/dist directory
---
+++ db/migrations/1694254385_item_table.go
@@ -0,0 +1,128 @@
+package migrations
+
+import (
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+	"github.com/pocketbase/pocketbase/models/schema"
+	"github.com/pocketbase/pocketbase/tools/types"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db)
+
+		ownerOnly := types.Pointer("@request.auth.id != '' && owners.id ?= @request.auth.id")
+		usersCollection, err := dao.FindCollectionByNameOrId("users")
+
+		if err != nil {
+			return err
+		}
+
+		collection := &models.Collection{
+			Name:       "items",
+			Type:       models.CollectionTypeBase,
+			ListRule:   ownerOnly,
+			ViewRule:   ownerOnly,
+			CreateRule: types.Pointer(""),
+			UpdateRule: ownerOnly,
+			DeleteRule: ownerOnly,
+			System:     false,
+			Schema: schema.NewSchema(
+				&schema.SchemaField{
+					Name:     "name",
+					Type:     schema.FieldTypeText,
+					Required: true,
+					System:   false,
+				},
+				&schema.SchemaField{
+					Name:     "date",
+					Type:     schema.FieldTypeDate,
+					Required: true,
+					System:   false,
+				},
+				&schema.SchemaField{
+					Name:   "lastForaged",
+					Type:   schema.FieldTypeText,
+					System: false,
+				},
+				&schema.SchemaField{
+					Name:     "owner",
+					Type:     schema.FieldTypeRelation,
+					Required: true,
+					System:   false,
+					Options: &schema.RelationOptions{
+						CollectionId:  usersCollection.Id,
+						CascadeDelete: false,
+						MinSelect:     types.Pointer(1),
+						MaxSelect:     types.Pointer(1),
+					},
+				},
+				&schema.SchemaField{
+					Name:     "sharedWith",
+					Type:     schema.FieldTypeRelation,
+					Required: false,
+					System:   false,
+					Options: &schema.RelationOptions{
+						CollectionId:  usersCollection.Id,
+						CascadeDelete: false,
+					},
+				},
+				&schema.SchemaField{
+					Name:     "lng",
+					Type:     schema.FieldTypeNumber,
+					Required: true,
+					System:   false,
+				},
+				&schema.SchemaField{
+					Name:     "lat",
+					Type:     schema.FieldTypeNumber,
+					Required: true,
+					System:   false,
+				},
+				&schema.SchemaField{
+					Name:   "colour",
+					Type:   schema.FieldTypeText,
+					System: false,
+				},
+				&schema.SchemaField{
+					Name:   "startMonth",
+					Type:   schema.FieldTypeText,
+					System: false,
+				},
+				&schema.SchemaField{
+					Name:   "endMonth",
+					Type:   schema.FieldTypeText,
+					System: false,
+				},
+				&schema.SchemaField{
+					Name:   "imageURL",
+					Type:   schema.FieldTypeText,
+					System: false,
+				},
+			),
+		}
+
+		err = dao.SaveCollection(collection)
+
+		if err != nil {
+			return err
+		}
+		return nil
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db)
+
+		collection, err := dao.FindCollectionByNameOrId("items")
+		if err != nil {
+			return err
+		}
+
+		err = dao.DeleteCollection(collection)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
